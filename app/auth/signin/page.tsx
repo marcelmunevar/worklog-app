1c27ef5da128115dfc0596ba97d580d74ac66115
Marcel Munevar <marcelmunevar@gmail.com>
2026-08-10 16:19:22 -0400

    setup github auth

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,24 +1,44 @@
-"use client";
-
 import {
   SignInPage,
   type AuthProvider,
   type AuthResponse,
 } from "@toolpad/core/SignInPage";
+import { AuthError } from "next-auth";
+import { signIn } from "@/auth";
 
 const providers: AuthProvider[] = [
   { id: "github", name: "GitHub" },
   { id: "google", name: "Google" },
 ];
 
-const signIn = async (provider: AuthProvider): Promise<AuthResponse> => {
-  console.log(`Sign in with ${provider.id}`);
-
-  return {
-    error: "This is a fake error",
-  };
-};
-
 export default function SignIn() {
-  return <SignInPage providers={providers} signIn={signIn} />;
+  return (
+    <SignInPage
+      providers={providers}
+      signIn={async (
+        provider: AuthProvider,
+        formData: FormData,
+        callbackUrl?: string,
+      ): Promise<AuthResponse> => {
+        "use server";
+
+        try {
+          await signIn(provider.id, {
+            redirectTo: callbackUrl ?? "/",
+          });
+
+          return {};
+        } catch (error) {
+          if (error instanceof AuthError) {
+            return {
+              error: "Unable to sign in.",
+              type: error.type,
+            };
+          }
+
+          throw error;
+        }
+      }}
+    />
+  );
 }
