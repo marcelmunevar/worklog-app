14fbfa3799cfc204392181d6d474829755aaf6d8
Marcel Munevar <marcelmunevar@gmail.com>
2026-08-10 15:51:54 -0400

    troubleshooting login page errors

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,38 +1,24 @@
 "use client";
-import * as React from "react";
-import { AppProvider } from "@toolpad/core/AppProvider";
+
 import {
-  AuthResponse,
   SignInPage,
   type AuthProvider,
+  type AuthResponse,
 } from "@toolpad/core/SignInPage";
-import { useTheme } from "@mui/material/styles";
 
-const providers = [
+const providers: AuthProvider[] = [
   { id: "github", name: "GitHub" },
   { id: "google", name: "Google" },
-  { id: "facebook", name: "Facebook" },
-  { id: "twitter", name: "Twitter" },
-  { id: "linkedin", name: "LinkedIn" },
 ];
 
-const signIn: (provider: AuthProvider) => void | Promise<AuthResponse> = async (
-  provider,
-) => {
-  const promise = new Promise<AuthResponse>((resolve) => {
-    setTimeout(() => {
-      console.log(`Sign in with ${provider.id}`);
-      resolve({ error: "This is a fake error" });
-    }, 500);
-  });
-  return promise;
+const signIn = async (provider: AuthProvider): Promise<AuthResponse> => {
+  console.log(`Sign in with ${provider.id}`);
+
+  return {
+    error: "This is a fake error",
+  };
 };
 
-export default function OAuthSignInPage() {
-  const theme = useTheme();
-  return (
-    <AppProvider theme={theme}>
-      <SignInPage signIn={signIn} providers={providers} />
-    </AppProvider>
-  );
+export default function SignIn() {
+  return <SignInPage providers={providers} signIn={signIn} />;
 }
